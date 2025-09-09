ed953374d9a4318d5fa0dd55acf2ec6c680bf82e
Lucas <luc.coulson6@gmail.com>
2025-09-08 20:32:21 -0400

    Now detects hand landmarks when hand is inside camera view

--- main.py
+++ main.py
@@ -4,11 +4,28 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision 
 
 
-capture = cv.VideoCapture(1)
+# Captures frames in camera and turns into video
+capture = cv.VideoCapture(0)
+
+# Accessing the hands object in mediapipe
+mp_hands = mp.solutions.hands 
+hand = mp_hands.Hands()
 
 while True:
     ret, img = capture.read()
     flipped = cv.flip(img, 1)
+
+
+    # Converts to RGB format and processes the hand
+    RGB_frame = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    result = hand.process(RGB_frame)
+    if result.multi_hand_landmarks:
+        for hand_landmark in result.multi_hand_landmarks:
+            print(hand_landmark)
+
+
+
+
 
 
 
